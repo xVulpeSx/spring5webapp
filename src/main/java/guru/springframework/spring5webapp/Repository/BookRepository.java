461912269dca484676b891fac2c4b25e8f9ce405
Vulpes <jeremi.lisek@gmail.com>
2020-01-20 14:26:56 +0100

    lesson 9 update - bootstrap

--- src/main/java/guru/springframework/spring5webapp/Repository/BookRepository.java
+++ src/main/java/guru/springframework/spring5webapp/Repository/BookRepository.java
@@ -4,5 +4,5 @@ import guru.springframework.spring5webapp.model.Book;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BookRepository extends CrudRepository<Book, Integer> {
-    
+
 }
